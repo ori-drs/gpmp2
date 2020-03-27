e5f52b39d2096213c3e06bcc73adf3530bb97a11
Mark Finean <mark.finean@stx.ox.ac.uk>
2020-03-27 20:25:08 +0000

    added state and scatter plots for planar arm

---
+++ mark_gpmp2/functions/plotStateEvolution.m
@@ -0,0 +1,23 @@
+function plotStateEvolution(result, delta_t, total_time_sec, total_time_step, handle)
+%PLOTSTATEEVOLUTION Summary of this function goes here
+%   Detailed explanation goes here
+
+time_steps = 0:delta_t:total_time_sec;
+    conf_x_series = [];
+    conf_v_series = [];
+
+    for i=0:total_time_step
+        x_conf = result.atVector(gtsam.symbol('x', i));
+        conf_x_series = horzcat(conf_x_series, x_conf);
+        v_conf = result.atVector(gtsam.symbol('v', i));
+        conf_v_series = horzcat(conf_v_series, v_conf);
+    end
+    
+    plot(handle, time_steps, conf_x_series)
+    hold on;
+    plot(handle, time_steps, conf_v_series)
+    legend('x1','x2','v1','v2')
+    xlabel('Time (s)');
+    ylabel('State value');
+end
+
--- mark_gpmp2/functions/scatterPlotPlanarArm.m
+++ mark_gpmp2/functions/scatterPlotPlanarArm.m
@@ -1,4 +1,4 @@
-function h = scatterPlotPlanarArm(positions, color, alpha)
+function h = scatterPlotPlanarArm(position, color, edge_alpha, face_alpha, handle)
 %PLOTPLANARARM Plot Arm class in 2D
 %
 %   Usage: PLOTPLANARARM(arm, conf, color, width)
@@ -7,18 +7,32 @@ function h = scatterPlotPlanarArm(positions, color, alpha)
 %   @color  color string, use plot convention, e.g. 'r' is red
 %   @width  line width
 
-position = arm.forwardKinematicsPosition(conf);
+% position = arm.forwardKinematicsPosition(conf);
 position = position(1:2, :);
 position = [[0;0], position];
 
-h(1) = plot(position(1,:), position(2,:), style, 'LineWidth', width);
+joint_X = position(1,:);
+joint_Y = position(2,:);
 
-h(2) = plot(position(1,1:end-1), position(2,1:end-1), 'k.', 'MarkerSize', 20);
+interp_X = [];
+interp_Y = [];
+for i = 1:size(joint_X,2)-1
+    XI = linspace(joint_X(i),joint_X(i+1),10);
+    YI = interp1(joint_X(1,i:i+1),joint_Y(1,i:i+1) ,XI);
+    
+    interp_X = [interp_X, XI];
+    interp_Y = [interp_Y, YI];
 
-scatter(X,y1, ...
-        'MarkerEdgeColor',color, ...
-        'MarkerFaceColor','b', ...
-        'MarkerFaceAlpha', 0.5,...
-        'MarkerEdgeAlpha', 0.5)
+end
+
+% h(1) = plot(joint_X, joint_Y, style, 'LineWidth', width);
+% 
+% h(2) = plot(position(1,1:end-1), position(2,1:end-1), 'k.', 'MarkerSize', 20);
+
+scatter(handle, interp_X, interp_Y, ...
+        'MarkerEdgeColor', color, ...
+        'MarkerFaceColor', color, ...
+        'MarkerFaceAlpha', face_alpha,...
+        'MarkerEdgeAlpha', edge_alpha)
 end
 
