fb4d71bc274a9470bc3520c29b3c94dc0a7fca0e
Mark Finean <mark.finean@stx.ox.ac.uk>
2020-04-06 16:16:01 +0100

    index each factor to identify obstacle factors

--- mark_gpmp2/createFactorGraph.m
+++ mark_gpmp2/createFactorGraph.m
@@ -1,4 +1,4 @@
-function [graph,obs_graph, init_values] = createFactorGraph(dataset, start_conf,end_conf, start_vel, end_vel, avg_vel,...
+function [graph,obs_graph, init_values, obs_factor_inds_and_time] = createFactorGraph(dataset, start_conf,end_conf, start_vel, end_vel, avg_vel,...
                                     total_time_sec, total_check_step,total_time_step, delta_t, Qc_model, check_inter, ...
                                     arm, cost_sigma, epsilon_dist, use_GP_inter, ...
                                     joint_vel_limit_model, joint_vel_limit_vec, joint_vel_limit_thresh,flag_joint_vel_limit,...
@@ -10,7 +10,8 @@ function [graph,obs_graph, init_values] = createFactorGraph(dataset, start_conf,
     obs_graph = gtsam.NonlinearFactorGraph;
 
     init_values = gtsam.Values;
-
+    obs_factor_inds_and_time = [];
+    factor_ind = 0;
     for i = 0 : total_time_step
         key_pos = gtsam.symbol('x', i);
         key_vel = gtsam.symbol('v', i);
@@ -25,12 +26,14 @@ function [graph,obs_graph, init_values] = createFactorGraph(dataset, start_conf,
         if flag_joint_vel_limit
             graph.add(gpmp2.VelocityLimitFactorVector(key_vel, joint_vel_limit_model, ...
                 joint_vel_limit_vec, joint_vel_limit_thresh));
+            factor_ind = factor_ind + 1;
         end    
 
         % start/end priors
         if i==0
             graph.add(gtsam.PriorFactorVector(key_pos, start_conf, pose_fix));
             graph.add(gtsam.PriorFactorVector(key_vel, start_vel, vel_fix));
+            factor_ind = factor_ind + 1;
         end
 
         % GP priors and cost factor
@@ -41,10 +44,14 @@ function [graph,obs_graph, init_values] = createFactorGraph(dataset, start_conf,
             key_vel2 = gtsam.symbol('v', i);
             graph.add(gpmp2.GaussianProcessPriorLinear(key_pos1, key_vel1, ...
                 key_pos2, key_vel2, delta_t, Qc_model));
+            factor_ind = factor_ind + 1;
 
             % cost factor
             graph.add(gpmp2.ObstaclePlanarSDFFactorArm(...
                 key_pos, arm, dataset.sdf, cost_sigma, epsilon_dist));
+            factor_ind = factor_ind + 1;
+            obs_factor_inds_and_time = [obs_factor_inds_and_time, [factor_ind; i]];
+            
             obs_graph.add(gpmp2.ObstaclePlanarSDFFactorArm(...
                 key_pos, arm, dataset.sdf, cost_sigma, epsilon_dist));
 
@@ -56,6 +63,9 @@ function [graph,obs_graph, init_values] = createFactorGraph(dataset, start_conf,
                         key_pos1, key_vel1, key_pos2, key_vel2, ...
                         arm, dataset.sdf, cost_sigma, epsilon_dist, ...
                         Qc_model, delta_t, tau));
+                    factor_ind = factor_ind + 1;
+                    obs_factor_inds_and_time = [obs_factor_inds_and_time, [factor_ind; i]];
+            
                     obs_graph.add(gpmp2.ObstaclePlanarSDFFactorGPArm( ...
                         key_pos1, key_vel1, key_pos2, key_vel2, ...
                         arm, dataset.sdf, cost_sigma, epsilon_dist, ...
@@ -67,6 +77,7 @@ function [graph,obs_graph, init_values] = createFactorGraph(dataset, start_conf,
         if i==total_time_step
             graph.add(gtsam.PriorFactorVector(key_pos, end_conf, pose_fix));
             graph.add(gtsam.PriorFactorVector(key_vel, end_vel, vel_fix));
+            factor_ind = factor_ind + 1;
         end
 
     end
