3c12b8b68c205eacb3d4245de8514cf92608265d
Mark Finean <mark.finean@stx.ox.ac.uk>
2020-04-22 15:06:54 +0100

    Remove dynamic classes

--- gpmp2/obstacle/ObstacleCost.h
+++ gpmp2/obstacle/ObstacleCost.h
@@ -21,33 +21,6 @@
 
 namespace gpmp2 {
 
-/// hinge loss obstacle cost function not const sdf
-inline double hingeLossObstacleCost(const gtsam::Point3& point, SignedDistanceField& sdf,
-    double eps, gtsam::OptionalJacobian<1, 3> H_point = boost::none) {
-
-  gtsam::Vector3 field_gradient;
-  double dist_signed;
-  try {
-    dist_signed = sdf.getSignedDistance(point, field_gradient);
-  } catch (SDFQueryOutOfRange&) {
-    //std::cout << "[hingeLossObstacleCost] WARNING: querying signed distance out of range, "
-    //    "assume zero obstacle cost." << std::endl;
-    if (H_point) *H_point = gtsam::Matrix13::Zero();
-    return 0.0;
-  }
-
-  if (dist_signed > eps) {
-    // faraway no error
-    if (H_point) *H_point = gtsam::Matrix13::Zero();
-    return 0.0;
-
-  } else {
-    // outside but < eps or inside object
-    if (H_point) *H_point = -field_gradient.transpose();
-    return eps - dist_signed;
-  }
-}
-
 /// hinge loss obstacle cost function
 inline double hingeLossObstacleCost(const gtsam::Point3& point, const SignedDistanceField& sdf,
     double eps, gtsam::OptionalJacobian<1, 3> H_point = boost::none) {
