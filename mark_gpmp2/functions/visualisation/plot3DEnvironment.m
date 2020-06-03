ac47ca95d10170c8375ec2290dcf87c3c4ecd288
Mark Finean <mark.finean@stx.ox.ac.uk>
2020-06-03 14:35:01 +0100

    add face alpha

--- mark_gpmp2/functions/visualisation/plot3DEnvironment.m
+++ mark_gpmp2/functions/visualisation/plot3DEnvironment.m
@@ -1,4 +1,4 @@
-function p1 = plot3DEnvironment(dataset, X, Y, Z, face_col)
+function p1 = plot3DEnvironment(dataset, X, Y, Z, face_alpha, face_col)
 %PLOT3DENVIRONMENT Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -31,10 +31,16 @@ function p1 = plot3DEnvironment(dataset, X, Y, Z, face_col)
     % Plot
     surf1=isosurface(X, Y, Z, data, isovalue);
     p1 = patch(surf1, 'FaceColor', 'r');
-    if nargin ==5
+    if nargin > 5
         isonormals(X, Y, Z, data, p1);
 %         set(p1,'FaceColor',face_col,'EdgeColor','k'); % set the color, mesh and transparency level of the surface
-        set(p1, 'EdgeColor',face_col); % set the color, mesh and transparency level of the surface
+%         set(p1, 'EdgeColor',face_col); % set the color, mesh and transparency level of the surface
+        set(p1, 'FaceColor',face_col); % set the color, mesh and transparency level of the surface
+    end
+   
+    if nargin >=5
+        isonormals(X, Y, Z, data, p1);
+        set(p1,'FaceAlpha',face_alpha, 'EdgeAlpha',face_alpha); % set the color, mesh and transparency level of the surface
     end
 %         isonormals(X, Y, Z, data, p1);
 %         set(p1,'FaceColor','red','EdgeColor','none','FaceAlpha',0.1); % set the color, mesh and transparency level of the surface
